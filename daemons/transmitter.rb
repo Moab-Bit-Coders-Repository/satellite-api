0c516503ac67b8cd98351bd14f3f059a6a02a0b3
Ben Teitelbaum <bt@blockstream.com>
2018-12-13 10:09:07 -0800

    transition to :transmitting in the transaction

--- daemons/transmitter.rb
+++ daemons/transmitter.rb
@@ -6,17 +6,15 @@ require_relative '../models/init'
 loop do
   sendable_order = nil
   while sendable_order.nil? do
+    sleep 1
     Order.transaction do
       sendable_order = Order.where(status: :paid).order(bid_per_byte: :desc).first
-      sendable_order.update(:status => :transmitting, :upload_started_at => Time.now) if sendable_order
+      sendable_order.transmit! if sendable_order
     end
-    sleep 1
   end  
   
   if TRANSMIT_RATE_LIMIT
     sleep Float(sendable_order.message_size) / TRANSMIT_RATE_LIMIT
   end
-  sendable_order.send!
-
-  # TODO transmit the message to the satellite uplink stations
+  sendable_order.end_transmission!
 end
