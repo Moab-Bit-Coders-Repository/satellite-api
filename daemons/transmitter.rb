08ce7966b9286169b2b2b64afbd92b50337d1850
Ben Teitelbaum <bt@blockstream.com>
2018-12-18 15:54:11 -0800

    informational logging for transmitter

--- daemons/transmitter.rb
+++ daemons/transmitter.rb
@@ -1,23 +1,39 @@
 require 'active_record'
+
+require 'logger'
+logger = Logger.new(STDOUT)
+logger.level = Logger::INFO
+
 require_relative '../constants'
 require_relative '../models/init'
 
 # complete any old transmissions that could be stuck (e.g. by early termination of the transmitter daemon)
-Order.transmitting.each {|o| o.end_transmission!}
+Order.transmitting.each do |order|
+  logger.info "completing stuck transmission #{order.uuid}"
+  order.end_transmission!
+end
 
 # loop forever dequing the highest-priced paid order and piping it to the GNU radio FIFO
 loop do
   sendable_order = nil
   while sendable_order.nil? do
     sleep 1
+    
     Order.transaction do
       sendable_order = Order.where(status: :paid).order(bid_per_byte: :desc).first
-      sendable_order.transmit! if sendable_order
+      if sendable_order
+        logger.info "transmission start #{sendable_order.uuid}"
+        sendable_order.transmit!
+      end
     end
   end  
   
   if TRANSMIT_RATE_LIMIT
-    sleep Float(sendable_order.message_size) / TRANSMIT_RATE_LIMIT
+    transmit_time = Float(sendable_order.message_size) / TRANSMIT_RATE_LIMIT
+    logger.info "sleeping for #{transmit_time} while #{sendable_order.uuid} transmits"
+    sleep transmit_time
   end
+  
+  logger.info "transmission end #{sendable_order.uuid}"
   sendable_order.end_transmission!
 end
